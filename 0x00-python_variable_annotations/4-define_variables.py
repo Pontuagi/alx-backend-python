8a676174f8abe5b27e26d2499d790adec6481fc0
Pontuagi <benepontuagi@gmail.com>
2023-12-07 11:31:05 +0300

    define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+This modules contains annotated variables of specified values
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
