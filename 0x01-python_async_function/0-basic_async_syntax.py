7b0381747c5fba077a20648648a8ccbf79de62b1
Pontuagi <benepontuagi@gmail.com>
2023-12-11 14:26:26 +0300

    basics of async type annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,9 +6,10 @@ This module contains a function that define an asynchronous coroutine.
 
 import asyncio
 import random
+from typing import Union
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay=10) -> Union[float, None]:
     """
     Function to generate a random float delay between 0 and max_delay
     and wait for the generated delay
