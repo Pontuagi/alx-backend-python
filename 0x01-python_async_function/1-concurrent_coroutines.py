3d614ab2ba9c452c2416f908d09c6006d3af23a5
Pontuagi <benepontuagi@gmail.com>
2023-12-11 14:51:54 +0300

    concurrent type annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -20,7 +20,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
     - List[float]: List of delays in ascending order.
     """
-    delays = []
+    delays: List[float] = []
 
     tasks = [wait_random(max_delay) for _ in range(n)]
 
